c0918c411997e7ee3f8756bafb8842a40c7c380c
Millsky <millsky@swagBookPro.local>
2015-04-27 22:46:08 -0400

    updated callback uri

--- resources/app.js
+++ resources/app.js
@@ -2,7 +2,7 @@ var goDJ = angular.module('godj',['spotify','godj.controllers','godj.services','
 
 goDJ.config(function(SpotifyProvider,$stateProvider, $urlRouterProvider){
 	SpotifyProvider.setClientId('d89976bf7cbd46b1a181a224be5e1e42');
-	SpotifyProvider.setRedirectUri('http://127.0.0.1:54663/callback.html');
+	SpotifyProvider.setRedirectUri('http://millsky.github.io/meGusta/callback.html');
 	SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
 
 	
